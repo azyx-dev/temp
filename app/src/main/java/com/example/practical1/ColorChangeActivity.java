61daa40f7109bd137c7b0b11877ff74c13856a10
Aditya <adityaup6032@gmail.com>
2026-03-01 21:33:09 +0530

    patch

--- app/src/main/java/com/example/practical1/ColorChangeActivity.java
+++ app/src/main/java/com/example/practical1/ColorChangeActivity.java
@@ -1,7 +1,13 @@
 package com.example.practical1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class ColorChangeActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class ColorChangeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_change);
+
+        ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+        Button colorButton = findViewById(R.id.colorButton);
+        Random random = new Random();
+
+        colorButton.setOnClickListener(v -> {
+            int red = random.nextInt(256);
+            int green = random.nextInt(256);
+            int blue = random.nextInt(256);
+            mainLayout.setBackgroundColor(Color.rgb(red, green, blue));
+        });
     }
 }
--- app/src/main/java/com/example/practical1/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/practical1/ConstraintLayoutActivity.java
@@ -8,6 +8,6 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear);
+        setContentView(R.layout.activity_constraint);
     }
 }
